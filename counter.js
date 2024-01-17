2ff4a1f767d9a82ea62e37bafd55fc4f8d5d54b4
Ewan <ewan.hamon@gmail.com>
2024-01-17 17:47:23 +0100

    *chore: :tada: debut de projet front

---
+++ counter.js
@@ -0,0 +1,9 @@
+export function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1000000000))
+  setCounter(0)
+}
